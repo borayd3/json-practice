e0d9c076aec1e13408dc2cf9e123515de81b32cc
bora yd <ngx3fy@virginia.edu>
2024-05-02 23:56:09 +0000

    adding python script

---
+++ lab 8/chacon.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import json
+
+with open('/workspace/json-practice/data/schacon.repos.json', 'r') as file:
+    data = json.load(file)
+    
+
+for d in data[0:5]:
+   #sample = (d['name'], d['html_url'],d['updated_at'], d['visibility'])
+   name1 = (d['name'])
+   html1 = (d['html_url'])
+   updated1 = (d['updated_at'])
+   visi1 = (d['visibility'])
+   records1 = (name1 + ", " + html1 + ", " + updated1 + ", " + visi1+ " \n")
+   file1 = open('/workspace/json-practice/lab 8/chacon.csv', 'a')
+   file1.write(records1)
+    
+
+
+   
